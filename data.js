5e2f39c68e6a436158ac7b81cb296be1ff49338b
mrgamingartist4-cmyk <mrgamingartist4@gmail.com>
2026-09-19 16:45:35 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,129 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-livi",
+      "name": "livi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": -3.1234204380464305,
+        "pitch": 0.15249741816174378,
+        "fov": 1.1921271810260077
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6402421500377766,
+          "pitch": 0.1135292694788852,
+          "rotation": 0,
+          "target": "0-livi"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-family-livi-",
+      "name": "family livi ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 3.0375753334328888,
+        "pitch": 0.06223523892784577,
+        "fov": 1.1921271810260077
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09123604024490461,
+          "pitch": 0.1641947827475594,
+          "rotation": 0,
+          "target": "2-dining"
+        },
+        {
+          "yaw": 0.5732945287362714,
+          "pitch": 0.00899584910230189,
+          "rotation": 1.5707963267948966,
+          "target": "0-livi"
+        },
+        {
+          "yaw": 0.17275710951524736,
+          "pitch": 0.015795399812283506,
+          "rotation": 1.5707963267948966,
+          "target": "3-k1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dining",
+      "name": "dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-k1",
+      "name": "k1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
